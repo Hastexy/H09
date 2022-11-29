be165fd28a20922fd9fbd815d35afa436db01290
Hastexy <hrafnkell.orri.elvarsson@gmail.com>
2022-11-29 01:07:32 +0000

    virkandi basic main menu

--- testing/UI_Layer/main_menu_ui.py
+++ testing/UI_Layer/main_menu_ui.py
@@ -1,23 +1,20 @@
-from Logic_Layer.player_ll import PlayerLL
-from Models.player import Player
-from UI_Layer.playerui import PlayerUI
+#from Logic_Layer.player_ll import PlayerLL
+#from Models.player import Player
+#from UI_Layer.playerui import PlayerUI
 
-from Logic_Layer.logic_wrapper import Logic_Wrapper
+#from Logic_Layer.logic_wrapper import Logic_Wrapper
 
 
 class MainMenu_UI:
     def __init__(self):
-        self.logic_wrapper = Logic_Wrapper()
+        pass#self.logic_wrapper = Logic_Wrapper()
 
     def menu_output(self):
         print("main menu")
-        print(
-            "1. Tournament manager"
-        )  # Create - Leage/Tournament, Club, Team, Player, matches.
+        print("1. Tournament manager")  # Create - Leage/Tournament, Club, Team, Player, matches.
         print("2. Result manager")  # next menu - 1. register, 2. edit
-        print(
-            "3. View manager"
-        )  # Team info, Unplayed matches, Complete results, LeaderBoards.
+        print("3. View manager")  # Team info, Unplayed matches, Complete results, LeaderBoards.
+        print("q. Quit")
 
     def input_prompt(self):
         while True:
@@ -28,11 +25,14 @@ class MainMenu_UI:
                 print("Goodbye")
                 break
             elif command == "1":
-                menu = PlayerUI(self.logic_wrapper)
-                back_method = menu.input_prompt()
-                if back_method == "q":
-                    return "q"
+                print("===Tournament Manager===")
+                # menu = PlayerUI(self.logic_wrapper)
+                # back_method = menu.input_prompt()
+                # if back_method == "q":
+                #     return "q"
             elif command == "2":
-                pass
+                print("===Result Manager===")
+            elif command == "3":
+                print("===View manager===")
             else:
                 print("invalid input, try again")
---
+++ testing/main.py
@@ -0,0 +1,4 @@
+from UI_Layer.main_menu_ui import MainMenu_UI
+
+mainmenu = MainMenu_UI()
+mainmenu.input_prompt()
