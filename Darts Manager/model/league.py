b5c1f80f6ec57cb53f4d08dcf28df90f8c1601c5
Hastexy <hrafnkell.orri.elvarsson@gmail.com>
2022-12-05 11:41:02 +0000

    hægt að semi búa til league

---
+++ Darts Manager/model/league.py
@@ -0,0 +1,14 @@
+class League:
+    def __init__(self, id="x", name="", host="", phone="", start_date="", end_date="", team_amount="", rounds="") -> None:
+        self.id = id
+        self.name = name
+        self.host = host
+        self.phone = phone
+        self.start_date = start_date
+        self.end_date = end_date
+        self.team_amount = team_amount
+        self.round = rounds
+        self.matches = []
+        
+    def __str__(self) -> str:
+        return f"==========\nName: {self.name}\nHost: {self.host}\nPhone nr: {self.phone}\nSD: {self.start_date}\nED: {self.end_date}\nAmount of teams: {self.team_amount}\nRounds: {self.round}\n=========="
