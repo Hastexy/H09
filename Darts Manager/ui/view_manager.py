0a0359cc7058140962924f276be2686d8c82c41d
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2022-12-08 18:00:22 +0000

    lagaði heimskar innsláttavillur

--- Darts Manager/ui/view_manager.py
+++ Darts Manager/ui/view_manager.py
@@ -89,6 +89,7 @@ class View_Manager_UI:
                             f"Game:   {match.home_team.title()} VS {match.away_team.title()}\n"
                         )
                         for game in match.games:
+                            self.parse_leg_score(game)
                             print(
                                 f"{game.home_player.title():<25}{game.home_score:<4}{game.type:^4}{game.away_score}{game.away_player.title():>25}"
                             )
@@ -129,15 +130,15 @@ class View_Manager_UI:
 
     def parse_leg_score(self, game: object) -> None:
         if game.home_score == "0":
-            game.home_score == "0-0"
+            game.home_score = "0-0"
         elif game.home_score == "1":
             game.home_score = "1-0"
         else:
             game.home_score = "1-1"
 
         if game.away_score == "0":
-            game.away_score == "0-0"
+            game.away_score = "0-0"
         elif game.away_score == "1":
             game.away_score = "1-0"
         else:
-            game.home_score = "1-1"
+            game.away_score = "1-1"
