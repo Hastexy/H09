f9e3765e2cd55bcdbaf3442d1fb2e6330e2db512
kjarri007 <102475373+kjarri007@users.noreply.github.com>
2022-12-09 12:05:31 +0000

    fixed filenotfounderror in viewmanager

--- Darts Manager/ui/view_manager_ui.py
+++ Darts Manager/ui/view_manager_ui.py
@@ -73,9 +73,14 @@ class View_Manager_UI:
                 print(TOP)
                 print(f"║{'===Viewing Upcoming Matches===':^109}║")
                 print(BOT)
-                matches_sorted_by_date = self.logic_wrapper.get_unfinished_matches(
-                    league_id
-                )
+                try:
+                    matches_sorted_by_date = self.logic_wrapper.get_unfinished_matches(
+                        league_id
+                    )
+                except FileNotFoundError:
+                    print("Something went wrong in the database, please fix!")
+                    return
+
                 for date, matches in matches_sorted_by_date.items():
                     self.show_date(date)
                     # print("Games:")
@@ -131,10 +136,12 @@ class View_Manager_UI:
     def select_league_id(self, all_leagues: List[object]) -> None:
         while True:
             self.display_available_leagues(all_leagues)
-            print("""
+            print(
+                """
 ╔══════════════════════╗
 ║ Input "b" to go back ║
-╚══════════════════════╝\n""")
+╚══════════════════════╝\n"""
+            )
             league_id = input("Which league do you want to view (League ID)?: ")
             for league in all_leagues:
                 if league_id == str(league.id) or league_id == "b":
@@ -179,25 +186,30 @@ class View_Manager_UI:
         print(f"\n╔{'═'*4}╦{'═'*15}╦{'═'*99}╗")
         print(f"║{f'{id}.':^4}║{'TEAM NAME':^15}║ {team.name.title():<98}║")
         print(f"╚{'═'*4}╩{'═'*15}╩{'═'*99}╝")
-        #=============CLUTTERED VERSION Could fix with colorama===============
+        # =============CLUTTERED VERSION Could fix with colorama===============
         # print(f"\n╔{'═'*39}╦{'═'*14}╦{'═'*39}╦{'═'*14}╗")
         # print(f"║{STR_NAME:^39}║{STR_SSN:^14}║{STR_EMAIL:^39}║{STR_DOB:^14}║")
         # print(f"╠{'═'*15}╦{'═'*15}╦{'═'*7}╩{'═'*14}╩{'═'*16}╦{'═'*15}╦{'═'*6}╩{'═'*14}╣")
         # print(f"║{STR_PHONE:^15}║{STR_HOME_PHONE:^15}║{STR_ADDRESS:^39}║{STR_ROLE:^15}║{'#'*21}║")
         print(f"\n╔{'═'*39}╦{'═'*14}╦{'═'*39}╦{'═'*14}╦{'═'*10}╗")
-        print(f"║{STR_NAME:^39}║{STR_SSN:^14}║{STR_EMAIL:^39}║{STR_DOB:^14}║{STR_ROLE:^10}║")
-        #print(f"\n{STR_NAME:<35}{STR_PHONE:<12}{STR_SSN:<15}{STR_ADDRESS:<20}{STR_ROLE:<10}")
+        print(
+            f"║{STR_NAME:^39}║{STR_SSN:^14}║{STR_EMAIL:^39}║{STR_DOB:^14}║{STR_ROLE:^10}║"
+        )
+        # print(f"\n{STR_NAME:<35}{STR_PHONE:<12}{STR_SSN:<15}{STR_ADDRESS:<20}{STR_ROLE:<10}")
         for player in team.players:
             print(f"╠{'═'*39}╬{'═'*14}╬{'═'*39}╬{'═'*14}╬{'═'*10}╣")
-            print(f"║ {player.name.title():<38}║{player.ssn:^14}║ {player.email:<38}║{player.dob:^14}║{player.role.upper():^10}║")
-            
-        #=============CLUTTERED VERSION Could fix with colorama===============
+            print(
+                f"║ {player.name.title():<38}║{player.ssn:^14}║ {player.email:<38}║{player.dob:^14}║{player.role.upper():^10}║"
+            )
+
+        # =============CLUTTERED VERSION Could fix with colorama===============
         #     print(f"╠{'═'*15}╩{'═'*15}╩{'═'*7}╦{'═'*14}╦{'═'*16}╩{'═'*15}╩{'═'*6}╦{'═'*14}╣")
         #     print(f"║{player.name.title():^39}║{player.ssn:^14}║{player.email:^39}║{player.dob:^14}║")
         #     print(f"╠{'═'*15}╦{'═'*15}╦{'═'*7}╩{'═'*14}╩{'═'*16}╦{'═'*15}╦{'═'*6}╩{'═'*14}╣")
         #     print(f"║{player.phone:^15}║{player.home_phone:^15}║{player.address:^39}║{player.role:^15}║{'#'*21}║")
         # print(f"╚{'═'*15}╩{'═'*15}╩{'═'*39}╩{'═'*15}╩{'═'*21}╝")
-        print(f"╚{'═'*39}╩{'═'*14}╩{'═'*39}╩{'═'*14}╩{'═'*10}╝")                    
+        print(f"╚{'═'*39}╩{'═'*14}╩{'═'*39}╩{'═'*14}╩{'═'*10}╝")
+
     def parse_leg_score(self, game: object) -> None:
         if game.home_score == "0":
             game.home_score = "0-0"
